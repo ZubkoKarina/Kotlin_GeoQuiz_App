09f344ab03f0833f3d041d9c203367a32e3694db
Karina Zubko <karinazubko2017@gmail.com>
2023-07-24 10:26:34 +0300

    work with Toast

--- app/src/main/java/com/example/geoquiz_app/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz_app/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.geoquiz_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
@@ -16,7 +18,19 @@ class MainActivity : AppCompatActivity() {
         falseButton = findViewById(R.id.false_button)
 
         trueButton.setOnClickListener{view: View ->
-            //
+            Toast.makeText(
+                this,
+                R.string.correct_toast,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        falseButton.setOnClickListener{view: View ->
+            Toast.makeText(
+                this,
+                R.string.incorrect_toast,
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
 }
