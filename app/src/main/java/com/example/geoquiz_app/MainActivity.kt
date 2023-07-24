34cde368104681b7e48b4a981cbfbf83dc923132
Karina Zubko <karinazubko2017@gmail.com>
2023-07-24 11:21:04 +0300

    using and supplementing viewBinding

--- app/src/main/java/com/example/geoquiz_app/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz_app/MainActivity.kt
@@ -3,13 +3,22 @@ package com.example.geoquiz_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
 import android.view.View
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
+
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
+    private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
