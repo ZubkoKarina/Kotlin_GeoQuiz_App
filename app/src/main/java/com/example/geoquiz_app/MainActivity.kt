f9278a20b0f7128d9e29f5b532515feada4ab48e
Karina Zubko <karinazubko2017@gmail.com>
2023-07-24 10:40:39 +0300

    adding Snackbar elements

--- app/src/main/java/com/example/geoquiz_app/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz_app/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import android.view.View
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
@@ -18,18 +19,18 @@ class MainActivity : AppCompatActivity() {
         falseButton = findViewById(R.id.false_button)
 
         trueButton.setOnClickListener{view: View ->
-            Toast.makeText(
-                this,
+            Snackbar.make(
+                trueButton,
                 R.string.correct_toast,
-                Toast.LENGTH_SHORT
+                Snackbar.LENGTH_SHORT
             ).show()
         }
 
         falseButton.setOnClickListener{view: View ->
-            Toast.makeText(
-                this,
+            Snackbar.make(
+                falseButton,
                 R.string.incorrect_toast,
-                Toast.LENGTH_SHORT
+                Snackbar.LENGTH_SHORT
             ).show()
         }
     }
