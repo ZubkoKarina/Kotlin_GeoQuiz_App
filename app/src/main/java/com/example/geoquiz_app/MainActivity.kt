3af03e1130074e8c488e05f72217149b4963f370
Karina Zubko <karinazubko2017@gmail.com>
2023-07-25 11:14:36 +0300

    Overriding more lifecycle functions (MainActivity.kt)

--- app/src/main/java/com/example/geoquiz_app/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz_app/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.example.geoquiz_app
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.geoquiz_app.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 
+private const val TAG = "MainActivity"
+
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
     private val questionBank = listOf(
         Question(R.string.question_australia, true),
         Question(R.string.question_oceans, true),
@@ -21,18 +23,15 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        Log.d(TAG,  "onCreate(Bundle?) called")
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         binding.trueButton.setOnClickListener{view: View ->
             checkAnswer(true)
         }
-
         binding.falseButton.setOnClickListener{view: View ->
             checkAnswer(false)
         }
-
         binding.nextButton.setOnClickListener{
             currentIndex = (currentIndex + 1) % questionBank.size
             updateQuestion()
@@ -41,12 +40,31 @@ class MainActivity : AppCompatActivity() {
             currentIndex = (currentIndex - 1 + questionBank.size) % questionBank.size
             updateQuestion()
         }
-
         binding.questionTextView.setOnClickListener{view: View ->
             currentIndex = (currentIndex + 1) % questionBank.size
             updateQuestion()
         }
         updateQuestion()
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
     }
     private fun updateQuestion() {
         val questionTextResId = questionBank[currentIndex].textResId
@@ -71,3 +89,5 @@ class MainActivity : AppCompatActivity() {
 //During lifecycle, an activity transitions between four states:
 //Nonexistent - onCreate(...) -> Created -> onStart() -> Started - onResume() -> Resumed
 //           <- onDestory()              <- onStop()            <- onPause()
+//                               Entire lifetime         Visible lifetime        Foreground lifetime
+//                               (instance in memory)    (view visible to user)  (user interactive with this activity)
