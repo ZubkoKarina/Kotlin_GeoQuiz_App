71a55b05ea8cf4cc15a52fe52590ada9d813b60c
Karina Zubko <karinazubko2017@gmail.com>
2023-08-01 09:41:58 +0300

    Filling out the second activity’s layout (res/layout/activity_cheat.xml)

---
+++ app/src/main/java/com/example/geoquiz_app/CheatActivity.kt
@@ -0,0 +1,11 @@
+package com.example.geoquiz_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CheatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cheat)
+    }
+}
